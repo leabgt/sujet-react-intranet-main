d976be14c903c55d4076d5487654ca8fca4d929d
Kristel Ocampo <krist3locampo@gmail.com>
2023-01-09 16:03:27 +0100

    LoginForm

---
+++ client/src/assets/views/Login.jsx
@@ -0,0 +1,14 @@
+import { LoginForm } from "../components/forms/LoginForm";
+import { useState } from "react";
+
+
+const Login = () => {
+    
+    return (
+        <>           
+			<LoginForm/>
+        </>
+    )
+ }
+ 
+ export default Login;
